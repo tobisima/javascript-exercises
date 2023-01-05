59d836b4b7c9ba625a105f12bec105380168a2e7
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2023-01-06 01:02:53 +1000

    Complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  let arr = [1, 1];
+  for(let i = 0; i < num - 2 ; i++) {
+  arr.push(arr[arr.length - 2] + arr[arr.length -1]);
+  }
+  return arr[arr.length - 1];
 };
 
 // Do not edit below this line
