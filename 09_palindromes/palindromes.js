f4c2ac8b548831b5ce463e84b4b8de1b41063fc6
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2023-01-05 19:18:16 +1000

    Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const arr = string.toLowerCase()
+                    .split('')
+                    .filter(char => !(char === '!' || char === '.'|| 
+                                      char === ',' || char === ' '));
+                                 
+  return arr.join('') === arr.reverse().join('');
 };
 
 // Do not edit below this line
