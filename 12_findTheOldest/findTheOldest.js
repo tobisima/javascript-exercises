e7b497a1eea05139d3accd72d0bf3616f5b28c95
tobisima <glavdo@gmail.comgit config --global user.email glavdo@gmail.com>
2023-01-07 18:04:54 +1000

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+  array.map(person => {
+    if (!person.yearOfDeath) person.yearOfDeath = new Date().getFullYear();
+  });  
+  array.sort((a, b) =>
+  (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
+  return array[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
